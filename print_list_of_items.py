b3d66f57977d266905b8e0f437b25b57284801dd
Sweta Suman <swetasuman@Swetas-MacBook-Pro.local>
2022-05-16 12:45:29 -0400

    my code

---
+++ print_list_of_items.py
@@ -0,0 +1,4 @@
+''' define a list with 10 elements, these elements can be name of fruits, print all the elements using for loop'''
+fruits=["mango", "orange", "grapes", "banana", "apple", "pomogranete", "watermellon", "strawberry", "guava", "cherry"]
+for x in fruits:
+    print(x)
